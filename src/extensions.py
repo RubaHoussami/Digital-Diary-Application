855cded0fb10689508acc07078d21fd5ad47ef2f
RubaHoussami <houssamiruba@gmail.com>
2024-10-17 16:39:53 +0300

    Initial commit

---
+++ src/extensions.py
@@ -0,0 +1,10 @@
+from swagger import Swagger
+
+swagger = Swagger(template={
+    "swagger": "2.0",
+    "info": {
+        "title": "Digital Diary Application",
+        "description": "A smart digital diary for your daily updates",
+        "version": "0.0.1",
+    }
+})
